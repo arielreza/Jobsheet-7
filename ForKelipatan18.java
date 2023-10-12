5c32a866f85d904ad01ee7b5eee837544967662b
Arielreza <arielreza111@gmail.com>
2023-10-12 11:40:15 +0700

    update dikit

--- ForKelipatan18.java
+++ ForKelipatan18.java
@@ -18,7 +18,7 @@ public class ForKelipatan18 {
                 
         }
 
-    }           System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-                System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+    }   System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
 }
 }
